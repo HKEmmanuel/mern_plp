6b16f8b4dcdd00a4ce0c48748e613ba0cd0a26f6
HKEmmanuel <houndjinouemmanuel@gmail.com>
2025-11-28 04:56:15 +0000

    remplacement de l'url dans services/api.js

--- tutoring-platform-mvp/src/services/api.js
+++ tutoring-platform-mvp/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://mern-plp-1.onrender.com';
 
 // Create axios instance
 const api = axios.create({
